d1f73d1d4967bfed3b3cfc0e5d0d1c4b40a3d218
aisha bastawesy <77200491+aisha331@users.noreply.github.com>
2023-12-28 14:29:41 +0200

    Add files via upload

---
+++ blink detection and nlp.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import cv2
+from cvzone.FaceMeshModule import FaceMeshDetector
+from cvzone.PlotModule import LivePlot
+import cvzone
+import pyttsx3
+
+def speak(text, rate=150):  # Adjust the rate parameter for speed (default: 150)
+    engine = pyttsx3.init()
+    engine.setProperty('rate', rate)
+    engine.say(text)
+    engine.runAndWait()
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = FaceMeshDetector(maxFaces=1)
+    plotY = LivePlot(640, 360, [20, 50], invert=True)
+
+    idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
+    ratioList = []
+    blinkCounter = 0
+    eyeClosed = False
+    color = (255, 0, 255)
+
+    while True:
+        success, img = cap.read()
+        img, faces = detector.findFaceMesh(img, draw=False)
+
+        if faces:
+            face = faces[0]
+            for id in idList:
+                cv2.circle(img, face[id], 5, color, cv2.FILLED)
+
+            leftUp = face[159]
+            leftDown = face[23]
+            leftLeft = face[130]
+            leftRight = face[243]
+            lenghtVer, _ = detector.findDistance(leftUp, leftDown)
+            lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
+
+            cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
+            cv2.line(img, leftLeft, leftRight, (0, 200, 0), 3)
+
+            ratio = int((lenghtVer / lenghtHor) * 100)
+            ratioList.append(ratio)
+
+            if len(ratioList) > 3:
+                ratioList.pop(0)
+                
+            ratioAvg = sum(ratioList) / len(ratioList)
+
+            if ratioAvg < 35 and not eyeClosed:
+                blinkCounter += 1
+                color = (0, 200, 0)
+                eyeClosed = True
+                speak(f"Blink detected. Count is now {blinkCounter}.", rate=200)  # Adjust the rate for speed
+            elif ratioAvg >= 35:
+                eyeClosed = False
+
+            cvzone.putTextRect(img, f'Blink Count: {blinkCounter}', (50, 100), colorR=color)
+
+            imgPlot = plotY.update(ratioAvg, color)
+            img = cv2.resize(img, (640, 360))
+            imgStack = cvzone.stackImages([img, imgPlot], 2, 1)
+        else:
+            img = cv2.resize(img, (640, 360))
+            imgStack = cvzone.stackImages([img, img], 2, 1)
+
+        cv2.imshow("Image", imgStack)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
+g
